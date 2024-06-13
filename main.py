55c8e7615588a4e2c21861106c3665dc58a9d6c0
Raniel <raniel.paula@fatec.sp.gov.br>
2024-06-12 22:07:30 -0300

    Implementando técnica de "classificação"

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+from sklearn.calibration import LabelEncoder
+from sklearn.cluster import KMeans 
+from sklearn.metrics import classification_report, accuracy_score
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.discriminant_analysis import StandardScaler
 from data_loading import load_data
 from exploratory_analysis import descriptive_statistics, frequency_distribution, plot_gravity_distribution, correlation_heatmap
 from preprocessing import handle_missing_values, remove_outliers, encode_categorical, normalize_numeric
@@ -91,6 +97,53 @@ def evaluate_type_veicle_by_drive_license(df):
     df = df.drop(columns=['especie_veiculo'])
     return df
 
+def convert_severity(df):
+    # df['cod_severidade'] = df['cod_severidade'].to_string(index=False).strip().replace('\n','')
+    severity_categories = {
+        1: 'NAO FATAL',
+        2: 'FATAL',
+        3: 'SEM FERIMENTOS',
+        0: 'NAO INFORMADO'
+    }
+    df['cod_severidade'] = df['cod_severidade'].fillna(0)
+    df['cod_severidade'] = df['cod_severidade'].map(severity_categories)
+    return df
+
+def training_classification_model(df):
+    label_encoders = {}
+    for column in ['cod_severidade', 'faixa_etaria', 'categoria_cnh']:
+        le = LabelEncoder()
+        df[column] = le.fit_transform(df[column])
+        label_encoders[column] = le
+
+
+    # Seleção das colunas relevantes
+    X = df[[ 'condutor', 'cinto_seguranca', 'embreagues', 'declaracao_obito']]
+    y = df['cod_severidade']
+
+    # Normalização dos dados
+    scaler = StandardScaler()
+    X_scaled = scaler.fit_transform(X)
+    X_train, X_test, y_train, y_test = train_test_split(X_scaled, y, test_size=0.6, random_state=42)
+    print(len(X_scaled))
+
+    # Aplicação do K-means
+    knn = KNeighborsClassifier(n_neighbors=3,  weights='distance', algorithm='auto')
+    knn.fit(X_train, y_train)
+    y_pred = knn.predict(X_test)
+
+    # Avaliação do modelo
+    print("Accuracy:", accuracy_score(y_test, y_pred))
+    print("Classification Report:\n", classification_report(y_test, y_pred))
+
+    # Adicionando a previsão ao DataFrame original para análise
+    df['pred_severidade'] = knn.predict(scaler.transform(X))
+    df['pred_severidade'] = label_encoders['cod_severidade'].inverse_transform(df['pred_severidade'])
+
+
+
+
+
 
 df = convert_to_datetime(df)
 df = parse_sex_column_to_bool(df)
@@ -99,6 +152,7 @@ df = convert_seat_belt_to_bool(df)
 df = convert_drunkenness_to_bool(df)
 df = evaluate_age_range(df)
 df = evaluate_type_veicle_by_drive_license(df)
+df = convert_severity(df)
 
 
 print(df)
@@ -106,6 +160,8 @@ print(df.columns.tolist())
 print(df.dtypes)
 print(df.head(20))
 
+training_classification_model(df)
+print(df.head(10))
 
 # Análise Exploratória
 # descriptive_statistics(df)
